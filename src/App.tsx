2a6f8f4d0915ca6f3bc2c7d887c22d85d6cb79db
sidonweb <heysid88@gmail.com>
2025-02-27 22:01:00 +0530

    home component fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import { Features } from "./components/Features";
 import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
-import { Pricing } from "./components/Pricing";
+// import { Pricing } from "./components/Pricing";
 import { Services } from "./components/Services";
 import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
+// import { Team } from "./components/Team";
 import { Testimonials } from "./components/Testimonials";
 import ServicesPage from "@/pages/Services"
 import "./App.css";
@@ -39,8 +39,6 @@ function App() {
               <Services />
               <Cta />
               <Testimonials />
-              <Team />
-              <Pricing />
             </>
           }
         />
